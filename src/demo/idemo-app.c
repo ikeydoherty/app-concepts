00229c6e0a8e24c04194dcb16de8dccb9ac9a53a
Ikey Doherty <ikey.doherty@gmail.com>
2013-12-30 15:01:05 +0000

    demo: Add basic UI content
    We'll hook the buttons up to open files inside and outside of the sandbox,
    leveraging use of the IPortal framework. (Which, currently isn't written.)

--- src/demo/idemo-app.c
+++ src/demo/idemo-app.c
@@ -42,6 +42,35 @@ static void i_demo_app_class_init(IDemoAppClass *klass)
 
 static void i_demo_app_init(IDemoApp *self)
 {
+        GtkWidget *layout;
+        GtkWidget *markup;
+        GtkWidget *button;
+
+        gtk_container_set_border_width(GTK_CONTAINER(self), 15);
+
+        layout = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+        gtk_container_add(GTK_CONTAINER(self), layout);
+
+        /* Tell people what we're about. */
+        markup = gtk_label_new("<big><b>IDemoApp</b>\n"
+                "Demonstration of techniques employed within the app-concepts repo\n"
+                "This app should be run within isandbox</big>\n\n"
+                "<span color='darkgrey'>Demonstration by Ikey Doherty</span>");
+        gtk_label_set_use_markup(GTK_LABEL(markup), TRUE);
+        gtk_box_pack_start(GTK_BOX(layout), markup, TRUE, TRUE, 10);
+
+        /* Buttons currently non-functioning */
+        button = gtk_button_new_with_label("Open files inside sandbox");
+        gtk_box_pack_start(GTK_BOX(layout), button, FALSE, FALSE, 5);
+
+        button = gtk_button_new_with_label("Open files outside sandbox");
+        gtk_box_pack_start(GTK_BOX(layout), button, FALSE, FALSE, 5);
+
+        gtk_window_set_title(GTK_WINDOW(self), "IDemoApp");
+        gtk_window_set_position(GTK_WINDOW(self), GTK_WIN_POS_CENTER);
+        gtk_window_set_default_size(GTK_WINDOW(self), 300, 300);
+        g_signal_connect(self, "destroy", gtk_main_quit, NULL);
+        gtk_widget_show_all(GTK_WIDGET(self));
 }
 
 static void i_demo_app_dispose(GObject *object)
